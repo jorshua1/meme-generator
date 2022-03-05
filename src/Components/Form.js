1d600c53a497b622ff26a9c587d7298757651957
Jorshua Hernandez <jorshua321@gmail.com>
2022-03-05 16:44:34 -0500

    Se agrega funcion para obtener url del objeto

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,6 +1,16 @@
 import React from "react";
+import memesData from "../Data/memesData";
+import Preview from "./Preview";
 
 function Form() {
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const numAleatorio = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[numAleatorio].url
+    console.log(url)
+  }
+
   return (
     <div className="flex flex-col items-center w-screen h-auto">
       <div className="grid grid-cols-2 gap-8 justify-around w-11/12 mt-16 mb-10">
@@ -15,7 +25,7 @@ function Form() {
           placeholder="Bottom text"
         />
       </div>
-      <button className="bg-violet-700 mx-2 h-12 w-11/12 rounded-lg text-white font-medium text-lg tracking-tight">
+      <button onClick={getMemeImage} className="bg-violet-700 mx-2 h-12 w-11/12 rounded-lg text-white font-medium text-lg tracking-tight">
         Get a new meme image
       </button>
     </div>
